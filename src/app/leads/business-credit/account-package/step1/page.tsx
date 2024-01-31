7e2c6a1d1c28b948e32515ec86436b693596ed27
Your Name <you@example.com>
2024-01-31 21:22:06 +0700

    check commit

--- src/app/leads/business-credit/account-package/step1/page.tsx
+++
@@ -1,5 +0,0 @@
-import { AccountPackageStep1Form } from "@/components/form-controller/modules/six/account.pacakge.step1.form";
-
-export default function Page() {
-  return <AccountPackageStep1Form />;
-}
